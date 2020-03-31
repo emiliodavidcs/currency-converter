52566d01187f7ec0d2bc0d94f7645456439b6751
Emilio David Clemente Silmi <emiliodavidcs@gmail.com>
2020-03-31 08:19:54 +0200

    Populates rates data

---
+++ src/data/rates.js
@@ -0,0 +1,8 @@
+export default [
+  { source: "Euro", target: "US Dollar", rate: 1.2897 },
+  { source: "Euro", target: "Schweizer Franken", rate: 1.3135 },
+  { source: "Euro", target: "Britisch Pound", rate: 0.8631 },
+  { source: "US Dollar", target: "JPY", rate: 109.62 },
+  { source: "Schweizer Franken", target: "US Dollar", rate: 0.996 },
+  { source: "Britisch Pound", target: "CAD", rate: 1.7574 }
+];
